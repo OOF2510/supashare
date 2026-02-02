0f52808687a15883d9e892cd225ad5951672afee
oof2510 <64423517+OOF2510@users.noreply.github.com>
2026-02-02 16:27:36 -0500

    update build script, dockerfile and workflow

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.25-alpine AS builder
 
 WORKDIR /app
 
-RUN apk --no-cache add ffmpeg
+RUN apk --no-cache add ffmpeg upx
 
 COPY go.mod go.sum ./
 RUN go mod download
@@ -10,7 +10,8 @@ RUN go mod tidy
 
 
 COPY . .
-RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o main .
+RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -trimpath -o main .
+RUN upx --best --lzma ./main
 
 FROM alpine:3.19
 
--- build.bash
+++ build.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Get first argument as version
 if [ -z "$1" ]; then
   echo "Usage: $0 <version>"
   exit 1
@@ -11,8 +10,6 @@ VERSION=${1}
 PROJECT_NAME=$(basename "$PWD")
 DIR=$(pwd)
 
-DOTENV_EXISTS=$(ls .env 2>/dev/null || true)
-
 echo "=== Running gofmt, go mod download and go mod tidy... ==="
 gofmt -w .
 go mod download
@@ -20,27 +17,27 @@ go mod tidy
 
 echo "=== Building the project... ==="
 mkdir -p dist/${VERSION}
-go build -v -x -o dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64 .
+go build -v -ldflags="-s -w" -trimpath -o dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64 .
+
+echo "=== Compressing binary with UPX... ==="
+if command -v upx &> /dev/null; then
+    upx --best --lzma dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64
+else
+    echo "UPX not found, skipping compression"
+fi
+
 echo "=== Build finished. Setting executable permissions... ==="
 chmod +x dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64
 
 echo "=== Copying pages... ==="
 cp -rv pages/ dist/${VERSION}/pages/
-echo "=== Pages copied to dist/${VERSION}/pages/ ==="
-if [ -n "$DOTENV_EXISTS" ]; then
-  echo "=== .env file found. ==="
-  echo "=== Lnking .env file... ==="
+
+if [ -f .env ]; then
+  echo "=== Linking .env file... ==="
   ln -sf ${DIR}/.env ${DIR}/dist/${VERSION}/.env
-  echo "=== .env file symlinked to dist/${VERSION}/.env ==="
 fi
 
-echo "=== Build completed. Executable is located at dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64 ==="
+echo "=== Binary size: ==="
+ls -lh ./dist/${VERSION}/${PROJECT_NAME}-${VERSION}.x86_64
 
-# echo "bulding test"
-# cd test/
-# gofmt -w .
-# go mod download
-# go mod tidy
-# go build -v -x -o ../dist/test.x86_64 .
-# echo "Test is at dist/test.x86_64"
-# cd ..
+echo "=== Build completed ==="
